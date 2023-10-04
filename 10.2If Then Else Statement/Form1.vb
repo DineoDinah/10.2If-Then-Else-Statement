68aec868e62d5c6350000ce1e39acf38a5083b3c
Lenovo-User <Lenovo-User@DESKTOP-RNPFAS5>
2023-10-04 23:56:34 +0200

    Add project files.

---
+++ 10.2If Then Else Statement/Form1.vb
@@ -0,0 +1,29 @@
+﻿Public Class Form1
+	Private Sub BtnBet_Click(sender As Object, e As EventArgs) Handles BtnBet.Click
+		Dim myNum As Integer
+		Dim LuckyNum As Integer
+
+
+		myNum = InputBox("Enter your lucky number and click OK")
+		LuckyNum = TextBox2.Text
+
+
+
+		If myNum > LuckyNum Then
+
+			MsgBox("Your number is too big")
+
+		ElseIf myNum < LuckyNum Then
+
+			MsgBox("Your number is too small")
+
+		Else
+
+			MsgBox("Congratulations! You strike the lucky number")
+
+		End If
+
+
+
+	End Sub
+End Class
